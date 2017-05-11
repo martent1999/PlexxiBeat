db13d1af2a355103d267f84c8a2a19c6a09da439
Marten Terpstra <marten.terpstra@plexxi.com>
2017-05-11 11:38:37 -0400

    More cleanup, alignment fixes etc

--- PlexxiBeat.py
+++ PlexxiBeat.py
@@ -26,7 +26,7 @@ from subprocess import Popen, PIPE
 import argparse
 
 # Command line options exist to override all of these
-debug = 0
+debug = 1
 host = 'plx-control-a1-1.ilab.plexxi.com'
 user = 'admin'
 password = 'plexxi'
@@ -46,38 +46,43 @@ sys.path.append('/opt/rh/python27/root/usr/lib/python2.7/site-packages')
 from plexxi.core.api.session import CoreSession
 from plexxi.core.api.binding import PlexxiSwitchCpuStatistics, PlexxiSwitch, AlarmManager, PlexxiRing
 
-# 
+
+#
 # getSwitchPeers(switch)
 #
 # for a given switch, return all peer ports. Used to count how many peers a switch has
 #
 def getSwitchPeers(switch):
-    """                                                                                                                      
-    Prints a list of switch Peers                                                                                            
-    switchId - name or mac of switch, str 'All' for all switches                                                             
-                                                                                                                             
-    Goes through uplink (ring port) cable and prints the peer switch at the end of that cable                                
-    This gives a complete map of switch uplink port to port connectivity through the Ring                                    
-    """
+	"""
+	Prints a list of switch Peers
+	switchId - name or mac of switch, str 'All' for all switches
 
-    if args.debug:
-        print("debug: getAllSwitchFabics() returns:", switch.getAllSwitchFabrics())
-    if args.debug > 1:
-        print("debug2: ", time.strftime("%H:%M:%S"))
-    fabric = switch.getAllSwitchFabrics()[0]
-    if args.debug > 1:
-        print("debug2: ", time.strftime("%H:%M:%S"))
-    outports = fabric.getAllSwitchFabricOutPorts()
-    if args.debug > 1:
-        print("debug2: ", time.strftime("%H:%M:%S"))
-    totalFabricPorts = 0
-    totalPeerPorts = 0
-    for port in outports:
-        if not port.isAccessPort():
-            totalFabricPorts = totalFabricPorts + 1
-            peerPorts = port.getAllPeerSwitchPorts()
-            totalPeerPorts = totalPeerPorts + len(peerPorts)
-    return(totalPeerPorts)
+	Goes through uplink (ring port) cable and prints the peer switch at the end of that cable
+	This gives a complete map of switch uplink port to port connectivity through the Ring
+	"""
+
+	if args.debug > 1:
+		print("debug2: ", time.strftime("%H:%M:%S"))
+	fabric = switch.getAllSwitchFabrics()[0]
+	if args.debug > 1:
+		print("debug2: ", time.strftime("%H:%M:%S"))
+	outports = fabric.getAllSwitchFabricOutPorts()
+	if args.debug > 1:
+		print("debug2: ", time.strftime("%H:%M:%S"))
+	totalFabricPorts = 0
+	totalPeerPorts = 0
+	peerList = []
+	for port in outports:
+		if debug > 1:
+			print("debug: port = ", port)
+		if not port.isAccessPort():
+			totalFabricPorts = totalFabricPorts + 1
+			peerPorts = port.getAllPeerSwitchPorts()
+			if debug > 1:
+				print("debug: peerPorts = ", peerPorts)
+			totalPeerPorts = totalPeerPorts + len(peerPorts)
+	return (totalPeerPorts, totalFabricPorts)
+
 
 #
 # pushToElasticSearch(object)
@@ -85,32 +90,32 @@ def getSwitchPeers(switch):
 # Pushes a JSON object into Elastic Search
 #
 def pushToElasticSearch(jsonObject):
-    """
-    Takes a JSON object and inserts it into Elastic Search
-    """
+	"""
+	Takes a JSON object and inserts it into Elastic Search
+	"""
 
-    if (push):
-        if (debug):
-            print("debug: pushing data to:", eshost, "using username", esuser, "and password", espassword)
-            print("debug: data - ", jsonObject)
-        esurl = 'http://' + args.eshost + ':9200/plexxi-beat/external?pretty'
-        esuserpass = args.esuser + ':' + args.espassword
-        p= Popen([
-                args.curl,
-                '-XPOST',
-                '-u',
-                esuserpass,
-                '-d',
-                jsonObject,
-                esurl
-                ],
-                 stdin=PIPE,
-                 stdout=PIPE,
-                 stderr=PIPE)
-        output, err = p.communicate()
-        return(p.returncode)
-    else:
-        return(0)
+	if (push):
+		if (debug):
+			print("debug: pushing data to:", eshost, "using username", esuser, "and password", espassword)
+		esurl = 'http://' + args.eshost + ':9200/plexxi-beat/external?pretty'
+		esuserpass = args.esuser + ':' + args.espassword
+		p = Popen([
+			args.curl,
+			'-XPOST',
+			'-u',
+			esuserpass,
+			'-d',
+			jsonObject,
+			esurl
+		],
+			stdin=PIPE,
+			stdout=PIPE,
+			stderr=PIPE)
+		output, err = p.communicate()
+		return (p.returncode)
+	else:
+		return (0)
+
 
 #
 # getFabricInfo(plexxiFabric)
@@ -118,117 +123,125 @@ def pushToElasticSearch(jsonObject):
 # Collect info and stats for a specific fabric (element from PlexxiRing.getAll())
 #
 def getFabricInfo(plexxiFabric):
-    """
-    Get a set of fabric stats and basic info for a specific fabric element
-    return a JSON object with the data collected
-    """
+	"""
+	Get a set of fabric stats and basic info for a specific fabric element
+	return a JSON object with the data collected
+	"""
 
-    fabric = {}
-    fabric['fabric'] = plexxiFabric.getName()
-    fabric['whole'] = plexxiFabric.isWhole()
-    fabric['stable'] = plexxiFabric.isStable()
-    switchList = []
-    for sw in plexxiFabric.getAllPlexxiSwitchesInRing():
-        switchList.append(str(sw.getName()))
-    fabric['switches'] = {'count': len(switchList), 'names': switchList}
-    fabric['type'] = "plexxi-fabric"
-    fabric['@timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%S%z")
-    fabric['control'] = args.host
-    fabric = json.dumps(fabric)
-    return(fabric)
+	fabric = {}
+	fabric['fabric'] = plexxiFabric.getName()
+	fabric['whole'] = plexxiFabric.isWhole()
+	fabric['stable'] = plexxiFabric.isStable()
+	switchList = []
+	for sw in plexxiFabric.getAllPlexxiSwitchesInRing():
+		switchList.append(str(sw.getName()))
+	fabric['switches'] = {'count': len(switchList), 'names': switchList}
+	fabric['type'] = "plexxi-fabric"
+	fabric['@timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%S%z")
+	fabric['control'] = args.host
+	return (fabric)
+
 
 def getSwitchInfo(plexxiSwitch):
-    """
-    Get a set of switch stats and basic info for a given switch
-    return a JSON object with the data collected
-    """
+	"""
+	Get a set of switch stats and basic info for a given switch
+	return a JSON object with the data collected
+	"""
 
-    switch = {}
-    if args.debug > 1:
-        print("debug2: ", time.strftime("%H:%M:%S"))
-    switch['type'] = "plexxi-switch"
-    switch['@timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%S%z")
-    switch['name'] = plexxiSwitch.getName()
-    switch['status'] = plexxiSwitch.getStatus()
-    switch['hwrev'] = plexxiSwitch.getPlexxiHwRevision()
-    switch['ip-address'] = str(plexxiSwitch.getIpAddress())
-    switch['productcode'] = plexxiSwitch.getProductCode()
-    switch['lightrail'] = plexxiSwitch.getLightrailCount()
-    switch['fabric'] = str(plexxiSwitch.getPlexxiRing())
-    if args.debug > 1:
-        print("debug2: ", time.strftime("%H:%M:%S"))
-    cpustats = plexxiSwitch.showStatisticsLast(switchStatisticsType = 'CPU', format='CSV').split(",")
-    switch['cpu'] = {'1m': float(cpustats[10]), '5m': float(cpustats[11]), '10m': float(cpustats[12])}
-    memstats = plexxiSwitch.showStatisticsLast(switchStatisticsType = 'MEMORY', format='CSV').split(",")
-    switch['memory'] = {'free': int(memstats[9]), 'total': int(memstats[10])}
-    tempstats = plexxiSwitch.showStatisticsLast(switchStatisticsType = 'TEMPERATURE', format='CSV').split(",")
-    switch['temp'] = {'cpu': float(tempstats[12]), 'fan': float(tempstats[14]), 'power': float(tempstats[15])}
-    if args.debug > 1:
-        print("debug2: ", time.strftime("%H:%M:%S"))
-    switch['peers'] = {'count': int(getSwitchPeers(plexxiSwitch))}
-    if args.debug > 1:
-        print("debug2: ", time.strftime("%H:%M:%S"))
-    switch['software'] = str(plexxiSwitch.getSwitchSoftwareVersion())
-    switch['oper-status'] =str(plexxiSwitch.getOperationalStage())
-    switch['control'] = args.host
-    switch = json.dumps(switch)
+	switch = {}
+	if args.debug > 1:
+		print("debug2: ", time.strftime("%H:%M:%S"))
+	switch['type'] = "plexxi-switch"
+	switch['@timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%S%z")
+	switch['name'] = plexxiSwitch.getName()
+	switch['status'] = plexxiSwitch.getStatus()
+	switch['hwrev'] = plexxiSwitch.getPlexxiHwRevision()
+	switch['ip-address'] = str(plexxiSwitch.getIpAddress())
+	switch['productcode'] = plexxiSwitch.getProductCode()
+	switch['lightrail'] = plexxiSwitch.getLightrailCount()
+	switch['fabric'] = str(plexxiSwitch.getPlexxiRing())
+	if args.debug > 1:
+		print("debug2: ", time.strftime("%H:%M:%S"))
+	cpustats = plexxiSwitch.showStatisticsLast(switchStatisticsType='CPU', format='CSV').split(",")
+	switch['cpu'] = {'1m': float(cpustats[10]), '5m': float(cpustats[11]), '10m': float(cpustats[12])}
+	memstats = plexxiSwitch.showStatisticsLast(switchStatisticsType='MEMORY', format='CSV').split(",")
+	switch['memory'] = {'free': int(memstats[9]), 'total': int(memstats[10])}
+	tempstats = plexxiSwitch.showStatisticsLast(switchStatisticsType='TEMPERATURE', format='CSV').split(",")
+	switch['temp'] = {'cpu': float(tempstats[12]), 'fan': float(tempstats[14]), 'power': float(tempstats[15])}
+	if args.debug > 1:
+		print("debug2: ", time.strftime("%H:%M:%S"))
+	(totalPeers, totalFabricPorts) = getSwitchPeers(plexxiSwitch)
+	switch['peers'] = {'count': int(totalPeers)}
+	switch['fabricports'] = int(totalFabricPorts)
+	if args.debug > 1:
+		print("debug2: ", time.strftime("%H:%M:%S"))
+	switch['software'] = str(plexxiSwitch.getSwitchSoftwareVersion())
+	switch['oper-status'] = str(plexxiSwitch.getOperationalStage())
+	switch['control'] = args.host
 
-    return(switch)
+	return (switch)
+
 
 # Main
 
 if __name__ == '__main__':
 
-# Parse arguments. Use defaults as defined above
+	# Parse arguments. Use defaults as defined above
 
-  parser = argparse.ArgumentParser(prog='PlexxiBeat.py')
-  parser.add_argument('--host', help='hostname of Plexxi Control instance', default=host)
-  parser.add_argument('--user', help='user name for Plexxi Control instance', default=user)
-  parser.add_argument('--password', help='password for Plexxi Control user', default=password)
-  parser.add_argument('--push', type=int, help='do not push data into ElasticSearch', default=push)
-  parser.add_argument('--curl', help='localion of "curl", default '+curl, default=curl)
-  parser.add_argument('--esuser', help='ElasticSearch username', default=esuser)
-  parser.add_argument('--espassword', help='ElasticSearch password', default=espassword)
-  parser.add_argument('--eshost', help='ElasticSearch host name', default=eshost)
-  parser.add_argument('--debug', type=int, help='enable debug printing', default=debug)
+	parser = argparse.ArgumentParser(prog='PlexxiBeat.py')
+	parser.add_argument('--host', help='hostname of Plexxi Control instance', default=host)
+	parser.add_argument('--user', help='user name for Plexxi Control instance', default=user)
+	parser.add_argument('--password', help='password for Plexxi Control user', default=password)
+	parser.add_argument('--push', type=int, help='do not push data into ElasticSearch', default=push)
+	parser.add_argument('--curl', help='localion of "curl", default ' + curl, default=curl)
+	parser.add_argument('--esuser', help='ElasticSearch username', default=esuser)
+	parser.add_argument('--espassword', help='ElasticSearch password', default=espassword)
+	parser.add_argument('--eshost', help='ElasticSearch host name', default=eshost)
+	parser.add_argument('--debug', type=int, help='enable debug printing', default=debug)
 
-  args = parser.parse_args()
+	args = parser.parse_args()
 
-  if (args.debug):
-      print('debug: host =', args.host)
-      print('debug: user =', args.user)
-      print('debug: password =', args.password)
-      print('debug: debug =', args.debug)
-      print('debug: push =', args.push)
-      print('debug: eshost = ', args.eshost)
-      print('debug: esuser = ', args.esuser)
-      print('debug: espassword = ', args.espassword)
+	if (args.debug):
+		print('debug: host =', args.host)
+		print('debug: user =', args.user)
+		print('debug: password =', args.password)
+		print('debug: debug =', args.debug)
+		print('debug: push =', args.push)
+		print('debug: eshost = ', args.eshost)
+		print('debug: esuser = ', args.esuser)
+		print('debug: espassword = ', args.espassword)
 
-# construct the URL co connect to Plexxi Control and create a Session
+	# construct the URL co connect to Plexxi Control and create a Session
 
-  url = 'http://' + args.host + ':8080/PlexxiCore/api'
-  session = CoreSession.connect(url,args.user,args.password)
+	url = 'http://' + args.host + ':8080/PlexxiCore/api'
+	session = CoreSession.connect(url, args.user, args.password)
 
-# First, get fabrics
+	# First, get fabrics
 
-  for i in PlexxiRing.getAll():
+	for i in PlexxiRing.getAll():
 
-      fabric = getFabricInfo(i)
+		if (args.debug):
+			print("debug: collecting info for fabric:", i)
 
-      if (args.debug):
-          print("debug: collecting info for fabric:", fabric)
+		fabric = getFabricInfo(i)
 
-      rc = pushToElasticSearch(fabric)
+		if (args.debug):
+			print("debug: collected: ", fabric)
 
-# Next, get all switches and collect stuff for them
+		rc = pushToElasticSearch(json.dumps(fabric))
 
-  switches = PlexxiSwitch.getAll()
-  for sw in switches:
-      switch = getSwitchInfo(sw)
+	# Next, get all switches and collect stuff for them
 
-      if (args.debug):
-          print("debug: collecting info for switch:", switch)
+	switches = PlexxiSwitch.getAll()
 
-      rc = pushToElasticSearch(switch)
+	for sw in switches:
 
+		if (args.debug):
+			print("debug: collecting info for switch:", sw)
 
+		switch = getSwitchInfo(sw)
+
+		if (args.debug):
+			print("debug: collected: ", switch)
+
+		rc = pushToElasticSearch(json.dumps(switch))
